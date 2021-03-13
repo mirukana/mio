f426633b104604c2bd7669b23798c88e94920005
miruka <miruka@disroot.org>
2021-03-13 15:11:16 -0400

    setup.py: fix ruamel and pytest-clarity version

--- setup.py
+++ setup.py
@@ -37,24 +37,24 @@ setup(
     """),
     extras_require = {
         "dev": requires("""
-            flake8                >= 3.8.4,  < 4
-            flake8-bugbear        >= 20.1.4, < 21
-            flake8-colors         >= 0.1.6,  < 0.2
-            flake8-commas         >= 2.0.0,  < 3
-            flake8-comprehensions >= 3.3.0,  < 4
-            flake8-executable     >= 2.0.4,  < 3
-            flake8-isort          >= 4.0.0,  < 5
-            flake8-logging-format >= 0.6.0,  < 0.7
-            flake8-pie            >= 0.6.1,  < 0.7
-            flake8-quotes         >= 3.2.0,  < 4
-            matrix-synapse        >= 1.29.0, < 2
-            mypy                  >= 0.812,  < 0.900
-            pep8-naming           >= 0.11.1, <0.12
-            pytest                >= 6.2.1,  < 7
-            pytest-asyncio        >= 0.14.0, < 0.15
-            pytest-benchmark      >= 3.2.3,  < 4
-            pytest-clarity        >= 0.3.0,  < 0.4
-            ruamel.yaml           >= 0.16,   < 0.17
+            flake8                >= 3.8.4,   < 4
+            flake8-bugbear        >= 20.1.4,  < 21
+            flake8-colors         >= 0.1.6,   < 0.2
+            flake8-commas         >= 2.0.0,   < 3
+            flake8-comprehensions >= 3.3.0,   < 4
+            flake8-executable     >= 2.0.4,   < 3
+            flake8-isort          >= 4.0.0,   < 5
+            flake8-logging-format >= 0.6.0,   < 0.7
+            flake8-pie            >= 0.6.1,   < 0.7
+            flake8-quotes         >= 3.2.0,   < 4
+            matrix-synapse        >= 1.29.0,  < 2
+            mypy                  >= 0.812,   < 0.900
+            pep8-naming           >= 0.11.1,  < 0.12
+            pytest                >= 6.2.1,   < 7
+            pytest-asyncio        >= 0.14.0,  < 0.15
+            pytest-benchmark      >= 3.2.3,   < 4
+            pytest-clarity        >= 0.3.0a0, < 0.4
+            ruamel.yaml           >= 0.16.13, < 0.17
         """),
     },
 
